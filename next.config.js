3eb2b3f4d45c27b1f4cd03921795313ec4f125ba
reolch <reolch@github.com>
2024-12-31 21:36:49 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // 静的エクスポートを有効化
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',  // GitHub Pages用のパス設定
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  output: 'export',
   images: {
-    unoptimized: true,  // 静的エクスポート時の画像最適化を無効化
+    unoptimized: true,
   },
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
