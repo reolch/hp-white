0b8c2bf4569e76125377e5d012deaed332e8c1f4
reolch <reolch@github.com>
2025-01-02 22:57:33 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -3,17 +3,35 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-    path: process.env.NEXT_PUBLIC_BASE_PATH || '',
   },
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   trailingSlash: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [
+                require('postcss-url')({
+                  url: (asset) => {
+                    if (asset.url.startsWith('/')) {
+                      return `${process.env.NEXT_PUBLIC_BASE_PATH || ''}${asset.url}`;
+                    }
+                    return asset.url;
+                  },
+                }),
+              ],
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
